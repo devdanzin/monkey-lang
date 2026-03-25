281f12946245e9231845b5cb55a7f72dd4725295
Henry the Frog <henry.the.froggy@gmail.com>
2026-03-25 16:46:32 -0600

    test: 18 parser tests for all new syntax. 591 total.

--- projects/monkey-lang/src/parser.test.js
+++ projects/monkey-lang/src/parser.test.js
@@ -178,3 +178,78 @@ function checkLiteralExpression(expr, expected) {
     assert.equal(expr.value, expected);
   }
 }
+
+describe('New Syntax Parsing', () => {
+  it('parses for loop', () => {
+    const prog = parse('for (let i = 0; i < 10; i++) { x }');
+    assert.ok(prog.statements.length > 0);
+  });
+  it('parses for-in', () => {
+    const prog = parse('for (x in arr) { x }');
+    assert.ok(prog.statements.length > 0);
+  });
+  it('parses break', () => {
+    const prog = parse('break');
+    assert.ok(prog.statements.length > 0);
+  });
+  it('parses continue', () => {
+    const prog = parse('continue');
+    assert.ok(prog.statements.length > 0);
+  });
+  it('parses match', () => {
+    const prog = parse('match (x) { 1 => "a", _ => "b" }');
+    assert.ok(prog.statements.length > 0);
+  });
+  it('parses ternary', () => {
+    const prog = parse('x > 0 ? 1 : 0');
+    assert.ok(prog.statements.length > 0);
+  });
+  it('parses do-while', () => {
+    const prog = parse('do { x } while (true)');
+    assert.ok(prog.statements.length > 0);
+  });
+  it('parses destructuring let', () => {
+    const prog = parse('let [a, b] = [1, 2]');
+    assert.ok(prog.statements.length > 0);
+  });
+  it('parses compound assignment', () => {
+    const prog = parse('x += 1');
+    assert.ok(prog.statements.length > 0);
+  });
+  it('parses i++', () => {
+    const prog = parse('i++');
+    assert.ok(prog.statements.length > 0);
+  });
+  it('parses else-if', () => {
+    const prog = parse('if (x) { 1 } else if (y) { 2 } else { 3 }');
+    assert.ok(prog.statements.length > 0);
+  });
+  it('parses slice', () => {
+    const prog = parse('arr[1:3]');
+    assert.ok(prog.statements.length > 0);
+  });
+  it('parses default params', () => {
+    const prog = parse('fn(x, y = 10) { x + y }');
+    assert.ok(prog.statements.length > 0);
+  });
+  it('parses template literal', () => {
+    const prog = parse('`hello ${x}`');
+    assert.ok(prog.statements.length > 0);
+  });
+  it('parses null', () => {
+    const prog = parse('null');
+    assert.ok(prog.statements.length > 0);
+  });
+  it('parses array mutation', () => {
+    const prog = parse('arr[0] = 1');
+    assert.ok(prog.statements.length > 0);
+  });
+  it('parses <=', () => {
+    const prog = parse('a <= b');
+    assert.ok(prog.statements.length > 0);
+  });
+  it('parses &&', () => {
+    const prog = parse('a && b');
+    assert.ok(prog.statements.length > 0);
+  });
+});
