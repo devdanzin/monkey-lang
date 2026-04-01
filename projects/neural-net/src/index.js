29eaad6d1755c5f4cc0b4d0bdd679d26b5d6178e
Henry the Frog <henry.the.froggy@gmail.com>
2026-04-01 14:59:57 -0600

    T37 BUILD: RNN + LSTM layers with BPTT (161 tests)

--- projects/neural-net/src/index.js
+++ projects/neural-net/src/index.js
@@ -2,6 +2,7 @@ export { Matrix } from './matrix.js';
 export { sigmoid, relu, leakyRelu, tanh, softmax, linear, getActivation } from './activation.js';
 export { Dense } from './layer.js';
 export { Conv2D, MaxPool2D, Flatten } from './conv.js';
+export { RNN, LSTM } from './rnn.js';
 export { BatchNorm } from './batchnorm.js';
 export { Dropout } from './dropout.js';
 export { mse, crossEntropy, getLoss } from './loss.js';
